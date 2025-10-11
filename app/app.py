e9abcd2ef318e9ea171895f6ba6c6649d453cc9d
spuerta <puertasantiago2002@gmail.com>
2025-10-11 10:19:10 -0500

    fix: Fixed CSRF security issue

--- app/app.py
+++ app/app.py
@@ -7,9 +7,12 @@ Routes:
     (add, subtract, multiply, divide).
 """
 from flask import Flask, render_template, request
+from flask_wtf.csrf import CSRFProtect
 from app.calculator import sumar, restar, multiplicar, dividir
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 
 @app.route("/", methods=["POST"])
